6b21c489788881577ee2188effe1532bc7fc55ea
Corneliu Voiculescu <corneliu@voiculescu.org>
2025-10-15 10:48:32 +0300

    Changed create_sequence

--- db-service/src/main/resources/db/functions/create_sequence.sql
+++ db-service/src/main/resources/db/functions/create_sequence.sql
@@ -7,14 +7,13 @@ create procedure
     create_sequence_proc(seq_name text, schema_name text default 'public')
     language plpgsql as
 'BEGIN
-   IF
-NOT EXISTS (
-      SELECT 1 FROM pg_class c
-      JOIN pg_namespace n ON n.oid = c.relnamespace
-      WHERE c.relkind = ''S''
-        AND c.relname = seq_name
-        AND n.nspname = schema_name
-   ) THEN
-      EXECUTE format(''CREATE SEQUENCE public.%I'', seq_name);
-END IF;
+    IF
+        NOT EXISTS (SELECT 1
+                    FROM pg_class c
+                             JOIN pg_namespace n ON n.oid = c.relnamespace
+                    WHERE c.relkind = ''S''
+                      AND c.relname = seq_name
+                      AND n.nspname = schema_name) THEN
+        EXECUTE format(''CREATE SEQUENCE %I.%I'', schema_name, seq_name);
+    END IF;
 END;';
